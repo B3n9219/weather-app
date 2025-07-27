9e4c12e303be062a0c55330c05fe3499653bf7d2
B3n9219 <benkirk1441@gmail.com>
2025-07-27 20:48:15 +0100

    Created a basic Api Manager for the weather api

---
+++ src/ApiManager.js
@@ -0,0 +1,22 @@
+export class ApiManager {
+    constructor() {
+        this.apiKey = "6NCXL4P4T2WWKND8DQ7DKY9R8"
+        this.baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+    }
+    async getLocationData(location) {
+        const response = await fetch(`${this.baseURL}/${location}?key=${this.apiKey}`, { mode:"cors"})
+        return await response.json()
+    }
+    filterLocationData(data) {
+        console.log(data)
+        const conditions = data.currentConditions
+        return {
+            location: data.address,
+            time: conditions.datetime,
+            icon: conditions.icon,
+            temperature: 66.4,
+            rainChance: conditions.precipprob,
+            windSpeed: conditions.windspeed
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import {ApiManager} from "./ApiManager";
+
+const apiManager = new ApiManager()
+const data = await apiManager.getLocationData("london")
+console.log(apiManager.filterLocationData(data))
